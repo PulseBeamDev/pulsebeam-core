eeaca2cd5175805d01732fcf0b9ed0de5267623d
Lukas Herman <lherman.cs@gmail.com>
2024-11-10 14:53:08 -0500

    use ed25519-dalek and include generated files

--- server-sdk-core/src/lib.rs
+++ server-sdk-core/src/lib.rs
@@ -4,7 +4,7 @@ use alloc::string::{String, ToString};
 
 use anyhow::{self, Context};
 use chrono::{Duration, Utc};
-use ed25519_compact::SecretKey;
+use ed25519_dalek::{SigningKey, SECRET_KEY_LENGTH};
 use jwt_compact::{alg::Ed25519, prelude::*};
 use serde::{Deserialize, Serialize};
 
@@ -18,7 +18,8 @@ struct CustomClaims {
 pub fn create_token(id: &str, signing_key_hex: &str) -> anyhow::Result<String> {
     let signing_key_raw =
         hex::decode(signing_key_hex).with_context(|| "invalid signing_key format")?;
-    let signing_key = SecretKey::from_slice(&signing_key_raw)?;
+    let signing_key_bytes: [u8; SECRET_KEY_LENGTH] = signing_key_raw[..].try_into()?;
+    let signing_key = SigningKey::from_bytes(&signing_key_bytes);
 
     let time_options = TimeOptions::default();
     let header = Header::empty().with_key_id(id);
@@ -30,4 +31,20 @@ pub fn create_token(id: &str, signing_key_hex: &str) -> anyhow::Result<String> {
 
     let token_string = Ed25519.token(&header, &claims, &signing_key)?;
     Ok(token_string)
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Ok;
+
+    use super::*;
+
+    #[test]
+    fn test_create_token() -> anyhow::Result<()> {
+        create_token(
+            "347da29c4d3b4d2398237ed99dcd7eb8",
+            "61eb06aa1a3a4ef80dd2a77503e226cc9afb667bed2dde38b31852ac781ea68a",
+        )?;
+        Ok(())
+    }
 }
---
+++ server-sdk-node/test.mjs
@@ -0,0 +1,7 @@
+import { createToken } from "./index.js";
+
+const token = createToken(
+  "347da29c4d3b4d2398237ed99dcd7eb8",
+  "61eb06aa1a3a4ef80dd2a77503e226cc9afb667bed2dde38b31852ac781ea68a",
+);
+console.log("From native", token);
