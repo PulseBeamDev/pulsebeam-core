6490106a22fbe1d073e8bccf79820fc79d3b536e
Lukas Herman <lherman.cs@gmail.com>
2024-11-11 11:39:58 -0500

    use wasm_bindgen in the same file

--- server-sdk-core/src/lib.rs
+++ server-sdk-core/src/lib.rs
@@ -7,44 +7,106 @@ use chrono::{Duration, Utc};
 use ed25519_dalek::{SigningKey, SECRET_KEY_LENGTH};
 use jwt_compact::{alg::Ed25519, prelude::*};
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
+use wasm_bindgen::prelude::*;
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct CustomClaims {
-    #[serde(rename = "sub")]
-    subject: String,
-    // other fields...
+#[derive(Error, Debug)]
+pub struct AppError {
+    reason: String,
 }
 
-pub fn create_token(id: &str, signing_key_hex: &str) -> anyhow::Result<String> {
-    let signing_key_raw =
-        hex::decode(signing_key_hex).with_context(|| "invalid signing_key format")?;
-    let signing_key_bytes: [u8; SECRET_KEY_LENGTH] = signing_key_raw[..].try_into()?;
-    let signing_key = SigningKey::from_bytes(&signing_key_bytes);
+// Implement the Display trait from core
+impl core::fmt::Display for AppError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        // Assuming Diagnostics implements core::fmt::Display
+        write!(f, "{}", self.reason)
+    }
+}
 
-    let time_options = TimeOptions::default();
-    let header = Header::empty().with_key_id(id);
-    let claims = Claims::new(CustomClaims {
-        subject: "alice".to_string(),
-    })
-    .set_duration_and_issuance(&time_options, Duration::hours(1))
-    .set_not_before(Utc::now());
+// https://rustwasm.github.io/wasm-bindgen/reference/types/result.html
+impl Into<JsValue> for AppError {
+    fn into(self) -> JsValue {
+        JsValue::from(js_sys::Error::new(&self.reason))
+    }
+}
 
-    let token_string = Ed25519.token(&header, &claims, &signing_key)?;
-    Ok(token_string)
+impl From<anyhow::Error> for AppError {
+    fn from(value: anyhow::Error) -> Self {
+        Self {
+            reason: value.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct PeerClaims {
+    #[serde(rename = "sub")]
+    subject: String,
+    group_id: String,
+}
+
+#[wasm_bindgen]
+pub struct TokenOpts {
+    subject: String,
+    group_id: String,
+}
+
+// public fields will treated as copyable by wasm_bindgen:
+// https://github.com/rustwasm/wasm-bindgen/issues/1985
+#[wasm_bindgen]
+pub struct App {
+    id: String,
+    secret: String,
+}
+
+#[wasm_bindgen]
+impl App {
+    #[wasm_bindgen(constructor)]
+    pub fn new(id: String, secret: String) -> Self {
+        Self { id, secret }
+    }
+
+    pub fn create_token(&self, opts: TokenOpts) -> Result<String, AppError> {
+        let signing_key_raw =
+            hex::decode(&self.secret).with_context(|| "invalid secret format, expecting hex")?;
+        let signing_key_bytes: [u8; SECRET_KEY_LENGTH] = signing_key_raw[..]
+            .try_into()
+            .with_context(|| "invalid secret length")?;
+        let signing_key = SigningKey::from_bytes(&signing_key_bytes);
+
+        let time_options = TimeOptions::default();
+        let header = Header::empty().with_key_id(&self.id);
+        let claims = Claims::new(PeerClaims {
+            subject: opts.subject,
+            group_id: opts.group_id,
+        })
+        .set_duration_and_issuance(&time_options, Duration::hours(1))
+        .set_not_before(Utc::now());
+
+        let token_string = Ed25519
+            .token(&header, &claims, &signing_key)
+            .with_context(|| "failed to sign token")?;
+        Ok(token_string)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use alloc::borrow::ToOwned;
     use anyhow::Ok;
 
     use super::*;
 
     #[test]
     fn test_create_token() -> anyhow::Result<()> {
-        create_token(
-            "347da29c4d3b4d2398237ed99dcd7eb8",
-            "61eb06aa1a3a4ef80dd2a77503e226cc9afb667bed2dde38b31852ac781ea68a",
-        )?;
+        let app = App::new(
+            "347da29c4d3b4d2398237ed99dcd7eb8".to_owned(),
+            "61eb06aa1a3a4ef80dd2a77503e226cc9afb667bed2dde38b31852ac781ea68a".to_owned(),
+        );
+        app.create_token(TokenOpts {
+            subject: "alice".to_owned(),
+            group_id: "0".to_owned(),
+        })?;
         Ok(())
     }
 }
